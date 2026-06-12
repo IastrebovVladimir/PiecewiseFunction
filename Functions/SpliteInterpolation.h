21ecdc258c769b17cc020a4053d3ceedc530deac
IastrebovVladimir <159059876+IastrebovVladimir@users.noreply.github.com>
2026-06-12 15:19:30 +0300

    Update SpliteInterpolation.h

--- Functions/SpliteInterpolation.h
+++ Functions/SpliteInterpolation.h
@@ -50,10 +50,10 @@ PiecewiseFunction<T> BuildNaturalCubicSpline(const MutableArraySequence<double>&
     for (int i = 1; i < n - 1; ++i) {
         double ai = h.Get(i - 1);
         double bi = 2.0 * (h.Get(i - 1) + h.Get(i));
-        double di = h.Get(i);
+        double сi = h.Get(i);
 
         double Li = bi - ai * mu.Get(i - 1);
-        mu.Append(di / Li);
+        mu.Append(сi / Li);
         z.Append((alpha.Get(i) - ai * z.Get(i - 1)) / Li);
     }
 
